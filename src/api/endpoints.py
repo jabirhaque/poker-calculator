d6530c81b40e8da2ae54f69017b9ddc1ed859d10
jabirhaque <jabirhaque@hotmail.co.uk>
2024-09-28 14:25:12 +0100

    probability api initial version

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,10 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from src.model.card import Card
+from src.model.game import Game
+from src.service.calculator import probability
 
 app = Flask(__name__)
 
-@app.route('/name', methods=['GET'])
-def get_name():
-    return jsonify({'name': 'Jabir'})
+@app.route('/get_probability', methods=['GET'])
+def get_probability():
+    pocket = [Card(key) for key in request.args.getlist('pocket')]
+    community = [Card(key) for key in request.args.getlist('community')]
+    opponent_num = int(request.args.get('opponent_num'))
+    game = Game(pocket, community, opponent_num, [])
+    return jsonify({'probability': probability(game)[0]})
 
 if __name__ == '__main__':
     app.run()
